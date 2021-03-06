dfb0fc9d8ddaea3b07cb83298d0a78548a39e7c6
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-05 21:29:34 -0800

    rendermoves

--- react-tactoe/src/components/Game.js
+++ react-tactoe/src/components/Game.js
@@ -8,4 +8,37 @@ const Game = () => {
     const [history, setHistory] = useState([Array(9).fill(null)]);
 
     const [stepNumber, setStepNumber] = useState(0);
-}
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(history[stepNumber]);
+    const xO = xIsNext ? "X" : "O";
+
+    const handleCLick =(i) => {
+        const historyPoint = history.slice(0, stepNumber +1);
+        const current = historyPoint[stepNumber];
+        const squares= [...current];
+        
+        //will return if won or occupied
+        if (winner || squares[i]) return;
+
+        // square seleciton
+        squares[i] = xO;
+        setHistory([...historyPoint, squares]);
+        setStepNumber(historyPoint.length);
+        setXisNext(!xIsNext);
+    };
+
+    const renderMoves = ()
+
+    return (
+        <>
+            <h1> React Tac Toe - using Hooks </h1>
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div className="info-wrapper">
+
+            </div>
+
+            <h3>History</h3>
+
+        </>
+    );
+};
