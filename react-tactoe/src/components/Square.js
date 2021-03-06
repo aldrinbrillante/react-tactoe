3969d743a6b18776a127842dfc6c155ff600f3be
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-03-05 21:08:17 -0800

    push css x/o

--- react-tactoe/src/components/Square.js
+++ react-tactoe/src/components/Square.js
@@ -2,10 +2,11 @@ import React from "react";
 
 //creating functional comp
 const Square = ({ value, onClick }) => {
+    const style = value ? `squares ${value}` : `squares`;
 
     return (
         <button className={style} onClick={onClick}>
           {value}
         </button>
-      );
-    };
+      )
+    }
